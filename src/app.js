ac4899bc0436ad67eb1289328a74b376a0f85f73
Rhys Botfield <rhys@sproutdesk.co.uk>
2022-11-10 14:30:52 +0000

    WIP - Change to OAuth Implied

--- src/app.js
+++ src/app.js
@@ -1,70 +1,122 @@
-import TwitchJs from 'twitch-js';
-import Alpine from 'alpinejs';
+import TwitchJs from "twitch-js";
+import Alpine from "alpinejs";
 
 const USERNAME = process.env.MIX_USERNAME;
-const ACCESS_TOKEN = process.env.MIX_ACCESS_TOKEN;
-const REFRESH_TOKEN = process.env.MIX_REFRESH_TOKEN;
 const CLIENT_ID = process.env.MIX_CLIENT_ID;
+const SCOPES = [
+    'bits:read',
+    'channel:read:redemptions',
+    'channel:read:subscriptions',
+];
+const TOPICS = [
+    'channel-bits-events-v2.<USER_ID>',
+    'channel-points-channel-v1.<USER_ID>',
+    'channel-subscribe-events-v1.<USER_ID>',
+];
+let match = null;
+if ((match = document.location.hash.match(/access_token=(\w+)/)) !== null && match.length > 1) {
+    localStorage.setItem('ACCESS_TOKEN', document.location.hash.match(/access_token=(\w+)/)[1]);
+    console.log(`ACCESS_TOKEN: ${localStorage.getItem('ACCESS_TOKEN')}`);
+}
+else if (localStorage.getItem('ACCESS_TOKEN') === null ) {
+    console.log(`No Access Token in storage, redirecting...`);
+    window.location = 'https://id.twitch.tv/oauth2/authorize'
+        + '?response_type=token'
+        + `&client_id=${CLIENT_ID}`
+        + '&redirect_uri=' + window.location.href
+        + '&scope=' + SCOPES.join('+');
+} else {
+    run ();
+}
 
-async function run () {
-    const USER = await fetch(`https://api.twitch.tv/helix/users?login=${USERNAME}`, {
-        headers: {
-            'Authorization': `Bearer ${ACCESS_TOKEN}`,
-            'Client-ID': CLIENT_ID
+async function run() {
+    //TODO Switch from "OAuth Client Credentials Flow" to "OIDC Implicit Code Flow"
+    // const ACCESS_TOKEN = await fetch("https://id.twitch.tv/oauth2/token", {
+    //     method: "POST",
+    //     body: new URLSearchParams({
+    //         client_id: CLIENT_ID,
+    //         client_secret: CLIENT_SECRET,
+    //         grant_type: "client_credentials",
+    //     }),
+    //     headers: {
+    //         "Content-Type": "application/x-www-form-urlencoded",
+    //     },
+    // })
+    //     .then((response) => response.json())
+    //     .then((body) => body.access_token);
+    // console.log(`ACCESS_TOKEN: ${ACCESS_TOKEN}`);
+
+    const USER = await fetch(
+        `https://api.twitch.tv/helix/users?login=${USERNAME}`,
+        {
+            headers: {
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+                "Client-ID": CLIENT_ID,
+            },
         }
-    }).then(response => response.json())
-    .then(body => body.data);
+    )
+        .then((response) => response.json())
+        .then((body) => body.data[0]);
 
-    const PubSub = new WebSocket('wss://pubsub-edge.twitch.tv');
+    console.log(USER);
+    console.log(`user: ${USER.id}`);
+
+    const PubSub = new WebSocket("wss://pubsub-edge.twitch.tv");
 
     const pingPongTimer = setInterval(() => {
-        PubSub.send(JSON.stringify({'type': 'PING'}));
+        PubSub.send(JSON.stringify({ type: "PING" }));
     }, 30000);
 
     const listenNonce = Math.random().toString(36).slice(2, 10);
-    PubSub.addEventListener('open', (event) => {
-        console.log('CONNECTION OPEN');
-        PubSub.send(JSON.stringify({
-            'type': 'LISTEN',
-            'nonce': listenNonce,
-            'data' : {
-                'topics': [
-                    `channel-bits-events-v2.${USER.id}`,
-                    `channel-points-channel-v1.${USER.id}`,
-                    `channel-subscribe-events-v1.${USER.id}`,
-                ],
-                'auth_token': ACCESS_TOKEN,
-            },
-        }));
+    PubSub.addEventListener("open", (event) => {
+        console.log("CONNECTION OPEN");
+        PubSub.send(
+            JSON.stringify({
+                type: "LISTEN",
+                nonce: listenNonce,
+                data: {
+                    topics: TOPICS.map(value => value.replace('<USER_ID>', USER.id)),
+                    auth_token: ACCESS_TOKEN,
+                },
+            })
+        );
     });
 
-    PubSub.addEventListener('message', (event) => {
-        console.log('MESSAGE RECEIVED');
+    PubSub.addEventListener("error", (error) => {
+        throw new Error(error);
+    });
+
+    PubSub.addEventListener("close", (event) => {
+        console.log("Websocket Closed");
+        clearInterval(pingPongTimer);
+    });
+
+    PubSub.addEventListener("message", (event) => {
+        console.log("MESSAGE RECEIVED");
         const message = JSON.parse(event.data);
         console.log(message);
 
-        if (message.type === 'PONG') {
+        if (message.type === "PONG") {
             return true;
         }
-        if (message.type === 'RESPONSE' && message.error !== "") {
-            throw new Error(message.error)
+
+        if (message.type === "RECONNECT") {
+            console.warn("Reconnecting Websocket");
+            clearInterval(pingPongTimer);
+            run();
+            return;
+        }
+
+        if (message.type === "RESPONSE" && message.error !== "") {
+            throw new Error(message.error);
         }
 
         if (message.type === "MESSAGE") {
-            switch (message.data.topic) {
-                case `channel-bits-events-v2.${USER.id}`:
-                    break;
-                case `channel-points-channel-v1.${USER.id}`:
-                    break;
-                case `channel-subscribe-events-v1.${USER.id}`:
-                    Alpine.
-                    break;
-                default:
-                    break;
-            }
+            let event = new CustomEvent(message.data.topic, {
+                detail: message.data.message,
+            });
+            return window.dispatchEvent(event);
         }
-
+        console.warn("Unknown message type received");
     });
 }
-run();
-
